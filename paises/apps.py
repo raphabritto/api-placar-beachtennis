51b8df41952b77d2ee25debb63282c10d9189345
Raphael Britto <raphael.britto@gmail.com>
2021-06-14 10:40:48 -0300

    exclusao projetos pais e estados

--- paises/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaisesConfig(AppConfig):
-    name = 'paises'
--- paises/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-# from paises.models import Paises
-
-
-# class PaisesSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Paises
-#         fields = '__all__'
